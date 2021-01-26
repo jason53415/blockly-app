45c572e3deb8a89c6d7a087b3a555ededfb9d058
You-Cheng Xiao <jason53415@gmail.com>
2021-01-27 07:03:18 +0800

    Correct options of file opening

--- js/generators/python/file.js
+++ js/generators/python/file.js
@@ -10,7 +10,7 @@ Blockly.Python['file_save'] = function(block) {
   var obj = Blockly.Python.valueToCode(block, 'OBJECT',
       Blockly.Python.ORDER_NONE) || 'None';
   var name = block.getFieldValue('FILENAME') + '.pickle';
-  var code = "with open('" + name + "', 'rb') as f:\n" + 
+  var code = "with open('" + name + "', 'wb') as f:\n" + 
       Blockly.Python.prefixLines("pickle.dump(" + obj + ", f)\n", Blockly.Python.INDENT);
   return code;
 };
@@ -21,7 +21,7 @@ Blockly.Python['file_load'] = function(block) {
   var obj = Blockly.Python.valueToCode(block, 'OBJECT',
       Blockly.Python.ORDER_NONE) || '_';
   var name = block.getFieldValue('FILENAME') + '.pickle';
-  var code = "with open('" + name + "', 'wb') as f:\n" + 
+  var code = "with open('" + name + "', 'rb') as f:\n" + 
       Blockly.Python.prefixLines(obj + " = pickle.load(f)\n", Blockly.Python.INDENT);
   return code;
 };
