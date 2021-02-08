8c28c613efc4b28e373699669e82a8e835d69a2a
You-Cheng Xiao <jason53415@gmail.com>
2021-02-09 07:46:01 +0800

    Add native python interpreter

---
+++ python/interpreter.py
@@ -0,0 +1,10 @@
+import sys
+import os
+
+if __name__ == '__main__':
+    multiprocessing.freeze_support()
+    script = sys.argv[1]
+    wd = os.path.dirname(script)
+    sys.path.append(wd)
+    sys.argv.pop(0)
+    exec(open(script).read(), globals(), locals())
