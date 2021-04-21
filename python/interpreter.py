6c1b01e17c76eb0a6195daee3746c2f4d5050c7d
You-Cheng Xiao <jason53415@gmail.com>
2021-04-21 08:55:03 +0800

    Avoid hello message printing from pygame

--- python/interpreter.py
+++ python/interpreter.py
@@ -7,4 +7,5 @@ if __name__ == '__main__':
     wd = os.path.dirname(script)
     sys.path.append(wd)
     sys.argv.pop(0)
+    os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "1"
     exec(open(script, "r", encoding='utf8').read(), globals(), locals())
