45a9557cf9df6b43993b0bb7b1b30c885a96a1be
You-Cheng Xiao <jason53415@gmail.com>
2021-08-26 18:28:16 +0800

    Integrate easy_game newly added to MLGame

---
+++ js/game_msg/easy_game.js
@@ -0,0 +1,31 @@
+Blockly.Msg["MLPLAY_GAME_STATUS_OPTIONS"] = [
+  ["%{BKY_EASY_GAME_GAME_STATUS_1}", "GAME_ALIVE"],
+  ["%{BKY_EASY_GAME_GAME_STATUS_2}", "GAME_PASS"],
+  ["%{BKY_EASY_GAME_GAME_STATUS_3}", "GAME_OVER"]
+];
+Blockly.Msg["MLPLAY_GET_INFO_OPTIONS"] = [
+  ["%{BKY_EASY_GAME_SCENE_INFO_1}", "scene_info['frame']"],
+  ["%{BKY_EASY_GAME_SCENE_INFO_2}", "scene_info['status']"],
+  ["%{BKY_EASY_GAME_SCENE_INFO_3}", "scene_info['ball_x']"],
+  ["%{BKY_EASY_GAME_SCENE_INFO_4}", "scene_info['ball_y']"],
+  ["%{BKY_EASY_GAME_SCENE_INFO_5}", "scene_info['score']"],
+  ["%{BKY_EASY_GAME_SCENE_INFO_6}", "scene_info['foods']"],
+  ["%{BKY_EASY_GAME_SCENE_INFO_7}", "scene_info"]
+];
+Blockly.Msg["MLPLAY_GET_CONSTANT_OPTIONS"] = [
+  ["%{BKY_EASY_GAME_CONSTANT_1}", "1/0"],
+  ["%{BKY_EASY_GAME_CONSTANT_2}", "2/800"],
+  ["%{BKY_EASY_GAME_CONSTANT_3}", "3/0"],
+  ["%{BKY_EASY_GAME_CONSTANT_4}", "4/600"],
+  ["%{BKY_EASY_GAME_CONSTANT_5}", "5/50"],
+  ["%{BKY_EASY_GAME_CONSTANT_6}", "6/50"],
+  ["%{BKY_EASY_GAME_CONSTANT_7}", "7/8"],
+  ["%{BKY_EASY_GAME_CONSTANT_8}", "8/8"]
+];
+Blockly.Msg["MLPLAY_RETURN_ACTION_OPTIONS"] = [
+  ["%{BKY_EASY_GAME_ACTION_1}", "['UP']"],
+  ["%{BKY_EASY_GAME_ACTION_2}", "['DOWN']"],
+  ["%{BKY_EASY_GAME_ACTION_3}", "['LEFT']"],
+  ["%{BKY_EASY_GAME_ACTION_4}", "['RIGHT']"],
+  ["%{BKY_EASY_GAME_ACTION_5}", "['NONE']"]
+];
